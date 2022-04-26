64663eb08e04cdfa1dddd468dfc5afd73b3391a4
Sullivan Xiong <38967557+SullivanXiong@users.noreply.github.com>
2022-04-26 14:50:01 -0700

    Todo Item component

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TodoItem from "./Todo/TodoItem";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoItem></TodoItem>
+    </div>
+  );
+}
+
+export default App;
